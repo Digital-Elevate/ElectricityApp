4a5584b99c88ce55a2269a8e0b4dc3a1934af6d8
ALAO LAWAL ADECHINA <lawalalaoad@gmail.com>
2024-05-18 17:47:55 +0200

    Update geocodingConfig.js

--- src/geocodingConfig.js
+++ src/geocodingConfig.js
@@ -1,5 +1,5 @@
 import Geocoder from 'react-native-geocoding';
 
-Geocoder.init('AIzaSyBIZHsIa0vuSctx3Z6dSSSzO4OnVGK4iKU'); // Remplacez par votre clé API Google Maps
+Geocoder.init('pi'); // Remplacez par votre clé API Google Maps
 
 export default Geocoder;
